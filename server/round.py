cf38eae3904ae7db08d19b80e56d8f459318167e
JAGANJAYACHANDRAN <jaganachu984@gmail.com>
2024-02-01 21:11:58 +0530

    files for server progress

---
+++ server/round.py
@@ -0,0 +1,56 @@
+import time as t
+from _thread import *
+from .game import Game
+from .chat import Chat
+
+
+class Round(object):
+    def __init__(self, word, player_drawing, players):
+        self.word = word
+        self.player_drawing = player_drawing
+        self.player_guessed = []
+        self.skips = 0
+        self.player_scores = {player: 0 for player in players}
+        self.time = 75
+        self.chat=Chat(self)
+        # self.start=time.time()
+        start_new_thread(self.time_thread, ())
+
+    def time_thread(self):
+        """
+        runs in thread to keep track of time
+
+        :return:None
+        """
+        while self.time > 0:
+            t.sleep(1)
+            self.time -= 1
+        self.end_round("Time is up")
+
+    def guess(self, player, wrd):
+        """
+        :return bool if player got it right
+        :param player:
+        :param wrd:
+        :return:
+        """
+        correct = wrd == self.word
+        if correct:
+            self.player_guessed.append(correct)
+        return wrd == self.word
+
+    def player_left(self, player):
+        """
+
+        :param player:Player
+        :return: None
+        """
+        if player in self.player_scores:
+            del self.player_scores[player]
+        if player in self.player_guessed:
+            self.player_guessed.remove(player)
+        if player == self.player_drawing:
+            self.end_round(" drawing player leaves ")
+
+    def end_round(self):
+        pass
